4c196a5e016fb6838933187ad0fdaaa84a012eb4
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-05-01 15:23:41 -0700

    Questions complete up through 6

--- js/app.js
+++ js/app.js
@@ -36,6 +36,45 @@ if (answer2 === 'yes' || answer2 === 'yeah' || answer2 === 'ya') {
     console.log('The user got the wrong answer to #2');
 }
 
+var answer3 = prompt('Lets learn a little about my creator, Nicklas Di Pietro! Are you interested?').toLowerCase();
+
+if (answer3 === 'yes' || answer3 === 'yeah' || answer3 === 'sure' || answer3 === 'ya') {
+    alert('Oh good I was really hoping you would be interested!');
+    console.log('The user agreed to move forward with question #3');
+} else {
+    alert('Well I bet youre a lot of fun at parties arent you?');
+    console.log('The user has declined to proceed with question #3. Sad :(');
+// Unaware how to end the prompts beyond this so that we can end the questionaire?
+// Could do the above if putting 4 onward in an if else statement as Nicholas put it. (research this to learn how)
+}
+
+var answer4 = alert('He is the youngest of four brothers. Four is pretty small. You humans arent as connected as us computers I suppose...');
+console.log('The user is learning from answer 4');
+
+var answer5 = prompt('Are you paying attention at all? This would be a lot of pointless rambling if not...').toLowerCase();
+
+if (answer5 === 'yes' || answer5 === 'yeah' || answer5 === 'sure' || answer5 === 'ya') {
+    alert('Oh good');
+    console.log('user has kept up with the process up to answer 5'); 
+} else {
+    prompt('Well do try to keep up. Ill forgive you this time but were on a time limit. The class is watching...creepy.');
+    console.log('The user has failed miserably for question 5');
+} 
+
+var answer6 = alert('He lived in Southern California for about 19 years in a small town called Whittier. Some guy named Richard Nixon grew up there I think? Oh and they did back to the future there too!');
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
